f90356f8a757aceed866d5baa5c5bcbc2c9b8ca6
ThevinduD <thevindud.work@gmail.com>
2025-05-25 21:22:13 +0530

    feat: implement create startup page

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,9 +1,19 @@
-const page = () => {
+import { auth } from "@/auth";
+import StartupForm from "@/components/StartupForm";
+import { redirect } from "next/navigation";
+
+const page = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
       </section>
+      <StartupForm />
     </>
   );
 };
