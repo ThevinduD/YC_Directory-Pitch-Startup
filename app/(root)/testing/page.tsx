4a647b610a5e98411a4aefb4fa5925d2fc11157b
ThevinduD <thevindud.work@gmail.com>
2025-06-11 13:52:05 +0530

    fix: MDEditor bug fixing

--- app/(root)/testing/page.tsx
+++ app/(root)/testing/page.tsx
@@ -7,11 +7,7 @@ export default function TestEditor() {
   const [value, setValue] = useState("");
   return (
     <div>
-      <MDEditor
-        height={300}
-        value={value}
-        onChange={(value) => setValue(value as string)}
-      />
+      <MDEditor value={value} onChange={(value) => setValue(value as string)} />
     </div>
   );
 }
